49a0790cd1d1b42c85bd418520e75e68075c7241
sagarsalyan <sagarslyn@gmail.com>
2021-06-08 13:29:52 +0000

    Added object spinner

---
+++ webapp/controls/ObjectSpinner.js
@@ -0,0 +1,51 @@
+sap.ui.define([
+	"sap/m/ObjectStatus"
+], function (ObjectStatus) {
+	"use strict";
+
+	return ObjectStatus.extend("App.CustomControls.controls.ObjectSpinner", {
+		metadata: {
+			properties: {
+				"spin": {
+					type: "boolean",
+					defaultValue: false
+				}
+			}
+		},
+		init: function () {
+
+			// define variable for control initial loading handling
+
+			this._bInitialLoading = true;
+
+			// execute standard control method
+
+			sap.m.ObjectStatus.prototype.init.apply(this, arguments);
+
+		},
+		setSpin: function (flag) {
+			this.setProperty("spin", flag);
+
+		},
+		onBeforeRendering: function () {
+			sap.m.ObjectStatus.prototype.onBeforeRendering.apply(this, arguments);
+		},
+		onAfterRendering: function () {
+			debugger
+			sap.m.ObjectStatus.prototype.onAfterRendering.apply(this, arguments);
+			if (this.getSpin()) {
+				var icon = this.$().find(".sapMObjStatusIcon");
+				if (icon.length > 0) {
+
+					icon.animate({
+						transform:'rotate(180deg)'
+					},5000);
+
+				}
+
+			}
+		},
+
+		renderer: "sap.m.ObjectStatusRenderer"
+	});
+});
